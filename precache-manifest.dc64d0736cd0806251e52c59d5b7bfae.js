d1972caf0d53ec5cf58dbdcd8e26a34e13474009
chaitanyakaranam <karanam.krishna.25@gmail.com>
2019-10-27 20:10:21 +0530

    Updates

---
+++ precache-manifest.dc64d0736cd0806251e52c59d5b7bfae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c87dc0c648f09f07f32ff3d16361d8",
+    "url": "/boardly/index.html"
+  },
+  {
+    "revision": "779d64e2f435207d3d81",
+    "url": "/boardly/static/css/main.71c34a3e.chunk.css"
+  },
+  {
+    "revision": "9da90ad174e41e0cf965",
+    "url": "/boardly/static/js/2.9772955e.chunk.js"
+  },
+  {
+    "revision": "779d64e2f435207d3d81",
+    "url": "/boardly/static/js/main.0de130e3.chunk.js"
+  },
+  {
+    "revision": "92200f255bd787894567",
+    "url": "/boardly/static/js/runtime-main.cfd1288c.js"
+  },
+  {
+    "revision": "de114294c906efb154989375e2052bd6",
+    "url": "/boardly/static/media/barchart.de114294.png"
+  },
+  {
+    "revision": "0cf7f3d2461564981d72f1a1d54d4fae",
+    "url": "/boardly/static/media/bottomLayer.0cf7f3d2.svg"
+  },
+  {
+    "revision": "20bb42a240a6fbb618ad2332f5687d96",
+    "url": "/boardly/static/media/dashboard.20bb42a2.svg"
+  },
+  {
+    "revision": "f44c495ad1209d319fcf5890a2d74a38",
+    "url": "/boardly/static/media/linechart.f44c495a.png"
+  },
+  {
+    "revision": "07d7d9bbec46bbc33c92dbd622a3ad26",
+    "url": "/boardly/static/media/mail.07d7d9bb.svg"
+  },
+  {
+    "revision": "3466f7f5158cfac67e8b0ad3089b477e",
+    "url": "/boardly/static/media/otherschart.3466f7f5.png"
+  },
+  {
+    "revision": "95403e952e26c1913e65efbe2bd9570b",
+    "url": "/boardly/static/media/topLayer.95403e95.svg"
+  }
+]);
